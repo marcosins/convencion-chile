979b389f510cddd1d6790cdf9e4a432eb1cca621
Marco RR <ext_mareyes@falabella.cl>
2021-10-08 22:36:22 -0300

    🐛 Verifica si existe argumento por consola antes de asignar

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const { parse } = require('node-html-parser');
 const { FILE_IN } = require('./config');
 const { getConvencionales } = require('./utils/convencion-parser.js');
 
-const FORMAT = process.argv[2].toUpperCase();
+const FORMAT = process.argv?.[2]?.toUpperCase();
 
 function toStdOut(err, data) {
   if (err) return console.error(err);
